228664a86c99233ee11332fb760dcb89450c736d
Alejandro García Sosa <99564159+alejandrogh7@users.noreply.github.com>
2022-10-30 15:17:24 -0500

    Delete first_project/target directory

--- first_project/target/debug/deps/first_project-f441bbd83a31b38a.d
+++
@@ -1,5 +0,0 @@
-/home/dev/Documents/rust-lng/first_project/target/debug/deps/first_project-f441bbd83a31b38a: src/main.rs
-
-/home/dev/Documents/rust-lng/first_project/target/debug/deps/first_project-f441bbd83a31b38a.d: src/main.rs
-
-src/main.rs:
--- first_project/target/debug/first_project.d
+++
@@ -1,1 +0,0 @@
-/home/dev/Documents/rust-lng/first_project/target/debug/first_project: /home/dev/Documents/rust-lng/first_project/src/main.rs
