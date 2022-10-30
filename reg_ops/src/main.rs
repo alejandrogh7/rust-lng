f6b87d16b89908f2b8a4679500fc60987d61ccbc
alejo1garciasosa@gmail.com <alejandrogh7>
2022-10-30 15:14:25 -0500

    r

--- reg_ops/src/main.rs
+++ reg_ops/src/main.rs
@@ -2,10 +2,57 @@ use regex::Regex;
 
 fn main() {
     // Regex
-    
+    let regex_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    let _regex_times = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+
     // User Data
+    println!("INSERT MATH EXPRESSION >> ");
+    let mut expression: String = String::new();
+    std::io::stdin().read_line(&mut expression).unwrap();
 
     // Validations
+    //times
+    loop {
+        let _regex_caps = _regex_times.captures(expression.as_str());
+        //debug var's
+        //println!("{:?}", _regex_caps);
 
+        if _regex_caps.is_none() {
+            break;
+        }
+
+        let _regex_caps = _regex_caps.unwrap();
+
+        let cap_expression = _regex_caps.get(0).unwrap().as_str();
+
+        let left_value: i32 = _regex_caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = _regex_caps.get(2).unwrap().as_str().parse().unwrap();
+
+        let times: i32 = left_value * right_value;
+
+        expression = expression.replace(cap_expression, &times.to_string());
+    }
+    //sum
+    loop {
+        let _regex_caps = regex_add.captures(expression.as_str());
+        //debug var's
+        //println!("{:?}", _regex_caps);
+
+        if _regex_caps.is_none() {
+            break;
+        }
+
+        let _regex_caps = _regex_caps.unwrap();
+
+        let cap_expression = _regex_caps.get(0).unwrap().as_str();
+
+        let left_value: i32 = _regex_caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = _regex_caps.get(2).unwrap().as_str().parse().unwrap();
+
+        let sum: i32 = left_value + right_value;
+
+        expression = expression.replace(cap_expression, &sum.to_string());
+    }
     // Show Result
+    println!("Result: {}", expression);
 }
