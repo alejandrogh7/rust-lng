42fe3060413079132dff75411c433b9083b99757
alejo1garciasosa@gmail.com <alejandrogh7>
2022-10-30 15:59:34 -0500

    las p

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,28 @@
+use csv::{ReaderBuilder, StringRecord};
+use str::{fs};
+
+const FILENAME: &str = "history.csv";
+
+struct DatoHistoria {
+    tipo_data: String,
+    texto: String,
+    tag: String,
+    vida: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_String(FILENAME).unwrap();
+
+    let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
+
+    for result in rdr.records() {
+        println!("{}", result.unwrap().get(2).unwrap().trim());
+    }
+
+    let dato = DatoHistoria {
+        tipo_data: "Situacion".to_string(),
+        texto: "text".to_String(),
+        tag: "INICIO".to_string(),
+        vida: 0,
+    }
 }
