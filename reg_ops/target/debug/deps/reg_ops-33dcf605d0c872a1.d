733d4c915598ee7b13cb131b4fbbec6f194f4ae4
Alejandro García Sosa <99564159+alejandrogh7@users.noreply.github.com>
2022-10-30 15:17:47 -0500

    Delete reg_ops/target directory

--- reg_ops/target/debug/deps/reg_ops-33dcf605d0c872a1.d
+++
@@ -1,5 +0,0 @@
-/home/dev/Documents/rust-lng/reg_ops/target/debug/deps/reg_ops-33dcf605d0c872a1: src/main.rs
-
-/home/dev/Documents/rust-lng/reg_ops/target/debug/deps/reg_ops-33dcf605d0c872a1.d: src/main.rs
-
-src/main.rs:
--- reg_ops/target/debug/reg_ops.d
+++
@@ -1,1 +0,0 @@
-/home/dev/Documents/rust-lng/reg_ops/target/debug/reg_ops: /home/dev/Documents/rust-lng/reg_ops/src/main.rs
