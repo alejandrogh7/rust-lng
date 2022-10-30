2a08fec23581670051d03dbc0681d298c7e448e2
alejo1garciasosa@gmail.com <alejandrogh7>
2022-10-30 15:43:01 -0500

    game

--- func/src/main.rs
+++ func/src/main.rs
@@ -1,3 +1,12 @@
+fn sum_one(arg: i32) -> i32 {
+    let final_num: i32 = arg + 1;
+    println!("{}", final_num);
+
+    return final_num;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let op: i32 = sum_one(10);
+    let op2: i32 = sum_one(op);
+    println!("{} and {}", op, op2);
 }
